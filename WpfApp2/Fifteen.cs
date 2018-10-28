fc01897854d679785ac83e1f4c87a13a332be5af
Maxim <Maxim>
2018-10-29 00:35:54 +0300

    Добавьте файлы проекта.

---
+++ WpfApp2/Fifteen.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    class Fifteen
+    {
+        int Size;  // Размер 
+        int[,] map;  // Массив координат.
+        int space_x, space_y; // Координаты пробела.
+        static Random Rand = new Random(); // Добавлем класс Random 
+        public Fifteen (int size)
+        {
+            if (size < 2) size = 2; // Проверка размера будущего массива 
+            if (size > 5) size = 5; // Проверка размера будущего массива 
+            this.Size = size; // Присваиваем размер 
+            map = new int[size,size]; // Массив равен размерам size на size 
+        }
+
+        public void Start() // Заполнение массива map 
+        {
+            for (int x = 0; x < Size; x++)
+                for (int y = 0; y < Size; y++)
+                    map[x, y] = Coords_to_position(x, y) + 1; // Заполнение массива
+            space_x = Size - 1;
+            space_y = Size - 1;
+            map[space_x, space_y] = 0; // Запоминаем место где у нас пробел
+        }
+
+        public void Shift (int position) // Перемещение "кнопок" 
+        {
+            int x, y; // Координаты кнопки
+            Position_to_coords(position, out x, out y); // Получаем Координаты  
+            if (Math.Abs(space_x - x) + Math.Abs(space_y - y) != 1) // Поверям расположение кнопки по сравнению с пустым местом 
+                return;
+            map[space_x, space_y] = map[x, y]; // Записываем координаты кнопки которую нажали 
+            map[x, y] = 0; // Записваем 0 кнопке которую нажали
+            space_x = x; // ЗАпоминаем где теперь пробел 
+            space_y = y; // Запоминаем где теперь пробел 
+        }
+
+        public void RandomShift() // Перемешивание 
+        {
+            Shift(Rand.Next(0, Size * Size));
+        }
+
+        public int Number(int Position) // Вычисление номера кнопки 
+        {
+            int x; // Координата X
+            int y; // Координата Y
+            Position_to_coords(Position, out x, out y); // Получение X,Y
+            if (x < 0 || x >= Size) return 0; // Проверяем, чтобы не было переполнения массива 
+            if (y < 0 || y >= Size) return 0; // Проверяем, чтобы не было переполнения массива
+            return map[x, y]; // Получение X,Y 
+        }
+        private int Coords_to_position(int x, int y) // Вычисление положения позиции 
+        {
+            return y * Size + x;
+        }
+
+        private void Position_to_coords(int position,out int x, out int y) // Вычисление положения координат 
+        {
+            x = position % Size;
+            y = position / Size;
+        }
+
+        public bool CheckWin() // Метод проверки кнопок на своих местах 
+        {
+            if (!(space_x == Size - 1 && space_y == Size - 1)) // Проверка пустой кнопки в правом нижнем углу 
+                return false;
+
+            for (int x = 0; x < Size; x++) // Проходим по всем элементам массива 
+                for (int y = 0; y < Size; y++) // Проходим по всем элементам массива 
+                    if (!(x == Size - 1 && y == Size - 1)) // Проверка если не последний вызов функции
+                    if (map[x, y] != Coords_to_position(x, y) + 1) // Проверка на соответвие позиций как при старте
+                        return false;
+            return true;
+        }
+    }
+}
---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp2
+{
+    public partial class MainWindow : Window
+    {
+        Fifteen Fifteen;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Fifteen = new Fifteen(4);
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            int Position = Convert.ToInt16(((Button)sender).Tag); // Переменная позиции, позицию получаем с помощью tag на кнопке 
+            Fifteen.Shift(Position); // Вызов метода смещения кнопки 
+            Refresh(); // Вызов метода визуальное обновления кнопок 
+            if (Fifteen.CheckWin()) // Проверка вызовом метода CheckWin
+            {
+                MessageBox.Show("Победа!!!");
+                Fifteen.Start();
+            }
+        }
+
+        private Button Button(int position) // Метод кнопок 
+        {
+            switch (position)
+            {
+                case 0: return Button0;
+                case 1: return Button1;
+                case 2: return Button2;
+                case 3: return Button3;
+                case 4: return Button4;
+                case 5: return Button5;
+                case 6: return Button6;
+                case 7: return Button7;
+                case 8: return Button8;
+                case 9: return Button9;
+                case 10: return Button10;
+                case 11: return Button11;
+                case 12: return Button12;
+                case 13: return Button13;
+                case 14: return Button14;
+                case 15: return Button15;
+                default: return null;
+            }
+        }
+
+        private void Game_start() // Запуск игры 
+        {
+            Fifteen.Start(); // Заупскаем заполнение массива
+            for (int j = 0; j < 100; j++) 
+                Fifteen.RandomShift(); // Выполняем перемешивание 
+            Refresh(); // Обновляем кнопки
+        }
+
+        private void Refresh() // Обновление всех 
+        {
+            for (int position = 0; position < 16; position++) // Для каждой позиции вызываем функцию Button
+            {
+                Button(position).Content = Fifteen.Number(position).ToString(); // Простовляем номер 
+                if (Button(position).Content.ToString() == "0") // Если "0", то присваем пусто 
+                {
+                    Button(position).Content = ""; 
+                }
+            }
+                
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Game_start();
+        }
+
+    }
+}
